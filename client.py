611e81e43738cef63576bd6fdf05dd0fc0fe4c84
Ruhaan Reddy <ruhaan3008@gmail.com>
2026-06-17 10:22:18 +0100

    Added better error handling to client

--- client.py
+++ client.py
@@ -3,26 +3,37 @@ import codes
 
 HOST = "127.0.0.1"
 PORT = 25545
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSock:
-    serverSock.connect((HOST, PORT))
-    print("Connection established")
-
-    serverSock.sendall(codes.SERVER_START_REQUEST)
-    data = serverSock.recv(1024)
-    while True:
-        data = serverSock.recv(1024)
-
-        if not data:
-            break
+def client_request():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSock:
+        try:
+            serverSock.connect((HOST, PORT))
+        except:
+            print("Error connecting with the server")
+            return
+            
+        print("Connection established")
         
-        print(data.decode())
+        try:
+            serverSock.sendall(codes.SERVER_START_REQUEST)
+            data = serverSock.recv(1024)
+            while True:
+                data = serverSock.recv(1024)
 
-        if (data == codes.SERVER_STARTING) or (data == codes.SERVER_RUNNING):
-            break
+                if not data:
+                    break
+            
+                print(data.decode())
 
-        elif data == codes.SERVER_ERROR:
-            print("Server Error! Server not running")
-            break
-        else:
-            serverSock.send(codes.IDLE)
+                if (data == codes.SERVER_STARTING) or (data == codes.SERVER_RUNNING):
+                    break
+
+                elif data == codes.SERVER_ERROR:
+                    print("Server Error! Server not running")
+                    break
+                else:
+                    serverSock.send(codes.IDLE)
+        except:
+            print("There was an error communicating")
+
+if (__name__ == "__main__"):
+    client_request()
